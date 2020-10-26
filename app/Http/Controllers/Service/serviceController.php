d445d994d7d5beb316e3321b832db7fd4fd9f18e
Thanathip Dungpimai <golfimrap@gmail.com>
2020-10-26 14:16:59 +0700

    update line

--- app/Http/Controllers/Service/serviceController.php
+++ app/Http/Controllers/Service/serviceController.php
@@ -255,7 +255,7 @@ class serviceController extends Controller
         $time_start = "08:30";
         $time_end = "16:30";
         
-        if ($eng_day != "Sunday" && $eng_day != "Sunday") {
+        if ($eng_day != "Saturday" && $eng_day != "Sunday") {
             if ($time >= $time_start && $time <= $time_end) {
                 $data_menuservice   =   $this->GetDataMenuService();
 
@@ -298,7 +298,7 @@ class serviceController extends Controller
                         define("MESSAGE","\nงานแจ้งซ่อม\nหมายเลขงาน ::".time().$request->building.$request->floor."\nวัน-เวลา แจ้งซ่อม :: ".formatDateThai(date('Y-m-d H:i:s'))."\nขื่อ - นามสกุล ".$request->name."\nหน่วยงาน :: ".$request->department."\nอาคาร :: ".$request->building." ชั้น :: ".$request->floor."\nแจ้งปัญหา :: ".$data_menuservice[$request->menuservice_id]."\nรายละเอียด :: ".nl2br($request->description)."\nเบอร์โทรศัพท์ :: ".$request->tel."\nกดรับงาน :: http://service.ddc.moph.go.th/service/admin/AdminViewService/".time().$request->building.$request->floor);
                         // define("MESSAGE","งานแจ้งซ่อม");
                         define("TOKEN","7D3DGDfaZEfMiQPjoJGAEtmhY6UQjHyJa2XNyvUJBBK");
-                        // define("TOKEN","DKzPT1p24lgNf56AWrFMKeoZSTHQm2OMkN53Hh5DW1Y");
+                        // define("TOKEN","DKzPT1p24lgNf56AWrFMKeoZSTHQm2OMkN53Hh5DW1Y"); //test
                         
                         $data = array(
                                     'message' => MESSAGE,
